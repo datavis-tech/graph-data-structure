28e6e37d94b69e8d64ed15a5cb4ef1f8484958f8
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-06-21 18:51:51 +0200

    feat: `hasCycle` now accepts options `sourceNodes` and `shouldFollow` to target specific sub-graph or cycles.

--- src/utils/hasCycle.spec.ts
+++ src/utils/hasCycle.spec.ts
@@ -3,14 +3,14 @@ import { Graph } from '../Graph.js';
 import { hasCycle } from './hasCycle.js';
 
 describe('hasCycle', () => {
-  it('Should detect cycle.', function () {
+  it('should detect cycle.', function () {
     const graph = new Graph();
     graph.addEdge('a', 'b');
     graph.addEdge('b', 'a');
     expect(hasCycle(graph)).toBe(true);
   });
 
-  it('Should detect cycle (long).', function () {
+  it('should detect cycle (long).', function () {
     const graph = new Graph();
     graph.addEdge('a', 'b');
     graph.addEdge('b', 'c');
@@ -19,15 +19,42 @@ describe('hasCycle', () => {
     expect(hasCycle(graph)).toBe(true);
   });
 
-  it('Should detect cycle (loop).', function () {
+  it('should detect cycle (loop).', function () {
     const graph = new Graph();
     graph.addEdge('a', 'a');
     expect(hasCycle(graph)).toBe(true);
   });
 
-  it('Should not detect cycle.', function () {
+  it('should not detect cycle.', function () {
     const graph = new Graph();
     graph.addEdge('a', 'b');
     expect(hasCycle(graph)).toBe(false);
+  });
+
+  it('should ignore the cycle in another sub-graph.', function () {
+    const graph = new Graph();
+    graph.addEdge('a', 'b');
+    graph.addEdge('b', 'c');
+    graph.addEdge('c', 'd');
+    graph.addEdge('d', 'a');
+
+    graph.addEdge('m', 'n');
+
+    expect(hasCycle(graph, { sourceNodes: ['m'] })).toBe(false);
+  });
+
+  it('should not detect the cycle when the traversing is stopped by the shouldFollow option.', function () {
+    const graph = new Graph<string, string>();
+    graph.addEdge('a', 'b', undefined, 'foo');
+    graph.addEdge('b', 'c', undefined, 'foo');
+    graph.addEdge('c', 'd', undefined, 'foo');
+    graph.addEdge('d', 'a', undefined, 'bar');
+
+    expect(
+      hasCycle(graph, {
+        shouldFollow: (source, target) =>
+          graph.getEdgeProperties(source, target) === 'foo',
+      }),
+    ).toBe(false);
   });
 });
--- src/utils/hasCycle.ts
+++ src/utils/hasCycle.ts
@@ -1,13 +1,21 @@
 import { depthFirstSearch } from '../algorithms/depthFirstSearch/index.js';
+import { DepthFirstSearchOptions } from '../algorithms/depthFirstSearch/types.js';
 import { CycleError } from '../CycleError.js';
 import { Graph } from '../Graph.js';
 
 /**
  * Perform a depth first search to detect an eventual cycle.
+ *
+ * You can provide a `shouldFollow` function to constrain the traversing and
+ * provide `sourceNodes` to explore a particular sub-graphs.
  */
-export function hasCycle(graph: Graph): boolean {
+export function hasCycle<Node, LinkProps>(
+  graph: Graph<Node, LinkProps>,
+  opts?: Pick<DepthFirstSearchOptions<Node, LinkProps>, 'shouldFollow' | 'sourceNodes'>,
+): boolean {
   try {
     depthFirstSearch(graph, {
+      ...opts,
       includeSourceNodes: true,
       errorOnCycle: true,
     });

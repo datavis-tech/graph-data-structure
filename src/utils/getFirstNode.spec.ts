08b6de887b1f2739b97d7a728ee03489dd19e2cf
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-06-21 17:54:16 +0200

    feat: add utility functions to retrieve node references
    findNodes, getNode, getFirstNode

---
+++ src/utils/getFirstNode.spec.ts
@@ -0,0 +1,36 @@
+import { describe, it, expect } from 'vitest';
+import { Graph } from '../Graph.js';
+import { getFirstNode } from './getFirstNode.js';
+import { getNode } from './getNode.js';
+
+describe('getFirstNode', () => {
+  type Node = { id: string; type: string };
+
+  it('should retrieve a node successfully when it exists in the graph', ({ expect }) => {
+    const graph = new Graph<Node>();
+    const node1 = { id: '1', type: 'foo' };
+    const node2 = { id: '2', type: 'foo' };
+
+    graph.addNode(node1);
+    graph.addNode(node2);
+
+    expect(getFirstNode(graph, (n) => n.id === '1')).toEqual(node1);
+    expect(getFirstNode(graph, (n) => n.id === '2')).toEqual(node2);
+  });
+
+  it('should throw when the node is not found', ({ expect }) => {
+    const graph = new Graph<Node>();
+    expect(() => getFirstNode(graph, (n) => n.id === 'nope')).toThrowError();
+  });
+
+  it('should not throw when more than one node is found', ({ expect }) => {
+    const graph = new Graph<Node>();
+    const node1 = { id: '1', type: 'foo' };
+    const node2 = { id: '2', type: 'foo' };
+
+    graph.addNode(node1);
+    graph.addNode(node2);
+
+    expect(getFirstNode(graph, (n) => n.type === 'foo')).toEqual(node1);
+  });
+});

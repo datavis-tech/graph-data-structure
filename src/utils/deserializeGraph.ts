36faa6a01be8af5b718bf4655a737746d6c339ff
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-06-25 15:05:51 +0200

    fix: require an identity function when deserializing a graph whose nodes are objects

--- src/utils/deserializeGraph.ts
+++ src/utils/deserializeGraph.ts
@@ -1,17 +1,35 @@
 import { Graph } from '../Graph.js';
 import { SerializedInput } from '../types.js';
 
-export function deserializeGraph<Node, LinkProps>(
-  data: SerializedInput<Node, LinkProps>,
+export function deserializeGraph<Node, LinkProps, NodeIdentity>(
+  ...args: Node extends object
+    ? [data: SerializedInput<Node, LinkProps>, identityFn: (node: Node) => NodeIdentity]
+    : [data: SerializedInput<Node, LinkProps>]
 ): Graph<Node, LinkProps> {
+  const [data, identityFn] = args;
+
   const g = new Graph<Node, LinkProps>();
+
+  const nodeIdentityMap = new Map<NodeIdentity, Node>();
 
   data.nodes.forEach((node) => {
     g.addNode(node);
+
+    if (identityFn) {
+      nodeIdentityMap.set(identityFn(node), node);
+    }
   });
 
   data.links.forEach((link) => {
-    g.addEdge.apply(g, [link.source, link.target, link.weight, link.props] as never);
+    if (!identityFn) {
+      g.addEdge.apply(g, [link.source, link.target, link.weight, link.props] as never);
+      return;
+    }
+
+    const source = nodeIdentityMap.get(identityFn(link.source)) ?? link.source;
+    const target = nodeIdentityMap.get(identityFn(link.source)) ?? link.target;
+
+    g.addEdge.apply(g, [source, target, link.weight, link.props] as never);
   });
 
   return g;

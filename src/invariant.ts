08930125db8f989d8bb954dc18694557ca803210
Curran <curran.kelleher@gmail.com>
2024-08-19 15:05:36 -0400

    Upgrade dependencies, run prettier

--- src/invariant.ts
+++ src/invariant.ts
@@ -1,7 +1,7 @@
 export function invariant(value: boolean, message?: string): asserts value;
 export function invariant<T>(
   value: T | null | undefined,
-  message?: string
+  message?: string,
 ): asserts value is T;
 export function invariant(value: any, message?: string) {
   if (value === false || value === null || typeof value === 'undefined') {

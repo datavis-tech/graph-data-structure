70e7bc8c92cfa05d0cc497cc53627565903c81c9
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-08-20 20:39:28 +0200

    build: remove js files

--- src/algorithms/depthFirstSearch/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.depthFirstSearch = void 0;
-var depthFirstSearch_js_1 = require("./depthFirstSearch.js");
-Object.defineProperty(exports, "depthFirstSearch", { enumerable: true, get: function () { return depthFirstSearch_js_1.depthFirstSearch; } });
--- src/invariant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.invariant = invariant;
-function invariant(value, message) {
-    if (value === false || value === null || typeof value === 'undefined') {
-        console.warn('Test invariant failed:', message);
-        throw new Error(message);
-    }
-}
